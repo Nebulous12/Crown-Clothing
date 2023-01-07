fd6ed0d1c0cb7f97a360b1edc5ea42f1960a6a58
Steve Daniel <stevedaniel2003@gmail.com>
2023-01-07 14:15:34 +0530

    Site Deployment

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
+import { useNavigate } from 'react-router-dom';
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import {CartDropdownContainer, EmptyMessage, CartItems} from  './cart-dropdown.styles.jsx';
+
+const CartDropdown = () => {
+  const navigate = useNavigate();
+  const navigateToCheckout = () =>{
+        navigate('/checkout')
+ 
+  }
+  const { cartItems } = useContext(CartContext);
+
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+     
+        {
+         cartItems.length ? (cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))) : (
+            <EmptyMessage>Your cart is empty :/</EmptyMessage>
+          )
+        }
+      </CartItems>
+      <Button onClick = {navigateToCheckout} >GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
+
+export default CartDropdown;
